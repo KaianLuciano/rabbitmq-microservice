4a129448a992b9d02674c5020044ca8f4f87c1b4
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-06-04 17:38:05 -0300

    Create Update TestController

--- src/main/java/com/rabbitmq/controller/TestController.java
+++ src/main/java/com/rabbitmq/controller/TestController.java
@@ -2,19 +2,32 @@ package com.rabbitmq.controller;
 
 import com.rabbitmq.config.QueueSender;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/teste")
-@RequiredArgsConstructor
 public class TestController {
-    private final QueueSender queueSender;
+    public TestController(AmqpTemplate queueSender) {
+        this.queueSender = queueSender;
+    }
+
+    private final AmqpTemplate queueSender;
 
     @GetMapping
     public String send(){
-        queueSender.send("test message");
+
+        String mensagem = "test message";
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("ultima", "sim");
+        Message message = new Message(mensagem.getBytes(), messageProperties);
+
+        queueSender.convertAndSend("teste-exchange", "routing-key-teste", message);
         return "ok. done";
     }
 
